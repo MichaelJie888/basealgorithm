54d98b694f0be310403e9337882e135cfed6f252
huangchuang <1125677925@qq.com>
2019-02-22 11:39:56 +0800

    二分法使用

---
+++ 双精度除2和整数除2的区别.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <string>
+using namespace std;
+const double eps=1e-5;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int A[10]={1,2,3,4,5,6,7,8,9,10};
+    double a=2,b=3;
+    cout<<(a+b)/2;
+//    for (int i = 0; i < 10; ++i) {
+//        cout<<A[i]<<endl;
+//    }
+    return 0;
+}
+
+//结果为2.5
